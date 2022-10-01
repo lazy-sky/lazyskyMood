b31bc72491a81999b7acc6e1c8dab0695efd3e98
lazy-sky <1cielo@naver.com>
2022-10-02 05:42:07 +0900

    Asset: Upload sounds

---
+++ src/assets/sounds/index.ts
@@ -0,0 +1,21 @@
+import oceanSound from './ocean.mp3'
+import rainSound from './rain.mp3'
+import whiteNoiseSound from './whiteNoise.mp3'
+import birdsSound from './birds.mp3'
+import keyboardSound from './keyboard.mp3'
+import fireplaceSound from './fireplace.mp3'
+import nightSound from './night.mp3'
+import cafeSound from './cafe.mp3'
+import windSound from './wind.mp3'
+
+export {
+  oceanSound,
+  rainSound,
+  whiteNoiseSound,
+  birdsSound,
+  keyboardSound,
+  fireplaceSound,
+  nightSound,
+  cafeSound,
+  windSound,
+}
