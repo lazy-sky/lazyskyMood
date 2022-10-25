915178ebbb0eb0cddae9cf5c954c9032f341c81f
lazy-sky <1cielo@naver.com>
2022-10-26 00:55:57 +0900

    Design: Change background with static image

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,7 +1,12 @@
+import backgroundImage1 from 'assets/images/background-lp.jpg'
+import backgroundImage2 from 'assets/images/background-headphone.jpg'
+
 import style from './background.module.scss'
 
 const Background = () => {
-  return <img src='https://picsum.photos/800?grayscale' alt='' className={style.background} />
+  const imageSrc = [backgroundImage1, backgroundImage2][Math.round(Math.random())]
+  // return <img src='https://picsum.photos/800?grayscale' alt='' className={style.background} />
+  return <img src={imageSrc} alt='' className={style.background} />
 }
 
 export default Background
